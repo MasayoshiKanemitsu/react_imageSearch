7685029ff1fbed94a066d241ffef47516ba09d44
kanemitsu <>
2022-11-09 00:06:53 +0900

    axiosでpixabay apiから画像配列を取得

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import SearchBar from "./components/SearchBar";
 
 const App = () => {
-  const onSearchSubmit = (term) => {
-    console.log(term); //入力してEnterを押した時の文字を出力
+  const [images, setImages] = useState([]);
+
+  const onSearchSubmit = async (term) => {
+    // console.log(term); //入力してEnterを押した時の文字を出力
+    try {
+      const params = {
+        key: "31180839-141fc33c2f41f224379d913bf",
+        q: term, //「q」は検索キーワードに入力された文字
+      };
+      const response = await axios.get("https://pixabay.com/api/", { params });
+      console.log(response);
+      setImages(response.data.hits); //検索でヒットした画像の配列をimagesにセット
+
+      //ヒットした画像がなかった場合
+      if (response.data.total === 0) {
+        window.alert("Not Found");
+      }
+    } catch {
+      window.alert("Error!"); //エラーが出た場合
+    }
   };
 
   return (
