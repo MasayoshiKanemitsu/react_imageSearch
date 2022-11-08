efb9007b53c912347d4e2fcfe1cc602b72535204
kanemitsu <>
2022-11-08 23:32:22 +0900

    リロード無効化処理追加

--- myapp/src/components/SearchBar.js
+++ myapp/src/components/SearchBar.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 const SearchBar = () => {
+  //inputをフォーカスした状態でEnterを押した時のリロードを無効化
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <>
-      <form action='' className='ui form'>
+      <form onSubmit={onFormSubmit} className='ui form'>
         <div className='field'>
-          <label htmlFor=''>Label</label>
-          <input type='text' name='' id='' />
+          <label htmlFor=''>Image Search for Pixabay</label>
+          <input type='text' name='' id='' placeholder='' />
         </div>
       </form>
     </>
